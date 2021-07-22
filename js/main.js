d3e4448241e146f06902ee52d6f7f57108887b45
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-07-22 12:16:29 -0300

    implementacao-math-jax

--- js/main.js
+++ js/main.js
@@ -241,19 +241,78 @@ window.addEventListener('resize', (e) => {
 })
 
 
-let iniciar = document.getElementById('iniciar');
+let tutorial = document.getElementById('tutorial');
 let acionar;
 let t = 0;
 
-iniciar.addEventListener('mouseenter', () => {
+tutorial.addEventListener('mouseenter', () => {
     acionar = setInterval( () => {
         t++;
-        iniciar.style.backgroundImage = `linear-gradient(${-45 + t*50}deg, #59bd87, #15c3f7, #ac92d0, #ffcd56, #75be6b)`;
+        tutorial.style.backgroundImage = `linear-gradient(${-45 + t*50}deg, #59bd87, #15c3f7, #ac92d0, #ffcd56, #75be6b)`;
     }, 100 );
 })
 
-iniciar.addEventListener('mouseleave', () => {
+tutorial.addEventListener('mouseleave', () => {
     clearInterval(acionar);
     t = 0;
-    iniciar.style.backgroundImage = 'linear-gradient(-45deg, #59bd87, #15c3f7, #ac92d0, #ffcd56, #75be6b)';
+    tutorial.style.backgroundImage = 'linear-gradient(-45deg, #59bd87, #15c3f7, #ac92d0, #ffcd56, #75be6b)';
 })
+
+
+
+tutorial.addEventListener('click', scrollToId);
+
+function scrollToId(event) {
+    event.preventDefault();
+    const element = event.target;
+
+    const lugar = getScrollTopByHref(element);
+
+    scrollToPosition(lugar);
+}
+
+function scrollToPosition(lugar){
+    smoothScrollTo(0, lugar, 1500);
+}
+
+function getScrollTopByHref(element){
+    const id = element.getAttribute('scroll');
+    return document.querySelector(id).offsetTop;
+}
+
+
+// Função para scroll suave com tempo de duração:
+
+
+/**
+ * Smooth scroll animation
+ * @param {int} endX: destination x coordinate
+ * @param {int} endY: destination y coordinate
+ * @param {int} duration: animation duration in ms
+ */
+
+ function smoothScrollTo(endX, endY, duration) {
+    const startX = window.scrollX || window.pageXOffset;
+    const startY = window.scrollY || window.pageYOffset;
+    const distanceX = endX - startX;
+    const distanceY = endY - startY;
+    const startTime = new Date().getTime();
+  
+    duration = typeof duration !== 'undefined' ? duration : 400;
+  
+    // Easing function
+    const easeInOutQuart = (time, from, distance, duration) => {
+      if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+      return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+    };
+  
+    const timer = setInterval(() => {
+      const time = new Date().getTime() - startTime;
+      const newX = easeInOutQuart(time, startX, distanceX, duration);
+      const newY = easeInOutQuart(time, startY, distanceY, duration);
+      if (time >= duration) {
+        clearInterval(timer);
+      }
+      window.scroll(newX, newY);
+    }, 1000 / 60); // 60 fps
+  };
